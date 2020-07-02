994a716db10f74eae91e53786fda6b59a4fed6f9
Oleg991 <o.n.eremenko@gmail.com>
2020-07-02 10:56:00 +0300

    Removed old unused code

--- Days 2.1/Extensions/UITableViewDelegate.swift
+++ Days 2.1/Extensions/UITableViewDelegate.swift
@@ -10,16 +10,6 @@ import UIKit
 
 extension MainViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        if let item = viewModel?.items?[indexPath.row]{
-//            do {
-//                try viewModel?.realm.write {
-//                    item.done = !item.done
-//                }
-//            } catch {
-//                print("Error saving task status, \(error)")
-//            }
-//        }
-//        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
