f4607460117e33970cb2f28e74cab90ec558c1de
Oleg991 <o.n.eremenko@gmail.com>
2022-01-05 19:01:50 +0300

    Moved previews to ViewController files

--- Days 2.1/Modules/Main/MainViewController.swift
+++ Days 2.1/Modules/Main/MainViewController.swift
@@ -146,3 +146,27 @@ private extension MainViewController {
         presenter?.addItemTapped()
     }
 }
+
+// MARK: - SwiftUI Preview
+#if DEBUG
+import SwiftUI
+
+struct MainVCRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let mainVC = MainViewController()
+        MainConfigurator.configure(with: mainVC)
+        return mainVC
+    }
+
+    func updateUIViewController(
+        _ uiViewController: UIViewController,
+        context: Context
+    ) {}
+}
+
+struct MainVCPreview: PreviewProvider {
+    static var previews: some View {
+        MainVCRepresentable()
+    }
+}
+#endif
