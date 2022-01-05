2e2dc07daf96471fb7d91f76cab9a52c9bd703a1
Oleg991 <o.n.eremenko@gmail.com>
2022-01-05 17:40:21 +0300

    Moved layout insets to a separate file

---
+++ Days 2.1/Support/Layout.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+struct Layout {
+    static let buttonWidth  = CGFloat(48)
+    static let defaultInset = CGFloat(16)
+    static let averageInset = defaultInset/2
+    static let smallInset   = defaultInset/4
+}
