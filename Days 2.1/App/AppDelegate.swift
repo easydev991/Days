f72e2e3695ddd2f5693d8c22f7b8fa16dc248841
Oleg991 <o.n.eremenko@gmail.com>
2022-01-05 17:54:25 +0300

    Refactor

--- Days 2.1/App/AppDelegate.swift
+++ Days 2.1/App/AppDelegate.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-@UIApplicationMain
+@main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
@@ -9,16 +9,12 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         true
     }
 
-    // MARK: UISceneSession Lifecycle
     func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        UISceneConfiguration(
-            name: "Default Configuration",
-            sessionRole: connectingSceneSession.role
-        )
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
 
