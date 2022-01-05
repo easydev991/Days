adefbe689d929586ff809d87189c91c550298530
Oleg991 <o.n.eremenko@gmail.com>
2022-01-05 17:54:46 +0300

    Added VC programmatically in SceneDelegate

--- Days 2.1/App/SceneDelegate.swift
+++ Days 2.1/App/SceneDelegate.swift
@@ -8,7 +8,13 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let mainVC = MainViewController()
+        let navigationController = UINavigationController(rootViewController: mainVC)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 }
 
