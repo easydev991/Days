e4f850b18551b7330b4ac470a59fe20a758cf37f
Oleg991 <o.n.eremenko@gmail.com>
2022-01-22 19:22:27 +0300

    Added a convenient enum for sorting

--- Days 2.1/Support/ItemSort.swift
+++ Days 2.1/Support/ItemSort.swift
@@ -2,7 +2,31 @@ enum ItemSort: String {
     case itemName, date
 }
 
+enum SortBy {
+    case dateAscending
+    case dateDescending
+    case titleAscending
+    case titleDescending
+}
+
 struct ItemSortModel {
     var sortBy: ItemSort
     var ascending: Bool
+
+    init(_ sort: SortBy) {
+        switch sort {
+        case .dateAscending:
+            sortBy = .date
+            ascending = true
+        case .dateDescending:
+            sortBy = .date
+            ascending = false
+        case .titleAscending:
+            sortBy = .itemName
+            ascending = true
+        case .titleDescending:
+            sortBy = .itemName
+            ascending = false
+        }
+    }
 }
