6922d9daecf36354088ce4e91e27de8f94e96e96
Oleg991 <o.n.eremenko@gmail.com>
2022-01-23 11:41:34 +0300

    Added extension for UIAlertController

---
+++ Days 2.1/Support/Extensions/UIAlertController+.swift
@@ -0,0 +1,38 @@
+import UIKit.UIAlertController
+
+extension UIAlertController {
+    enum ExitButton {
+        case cancel, close
+
+        var title: String {
+            self == .cancel
+            ? Text.Button.cancel.text
+            : Text.Button.close.text
+        }
+    }
+
+    static func makeAlertWith(
+        title: String? = nil,
+        message: String? = nil,
+        style: UIAlertController.Style,
+        actions: [UIAlertAction]? = nil,
+        exitButton button: ExitButton
+    ) -> UIAlertController {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: style
+        )
+        if let actions = actions {
+            actions.forEach {
+                alert.addAction($0)
+            }
+        }
+        let exit = UIAlertAction(
+            title: button.title,
+            style: .cancel
+        )
+        alert.addAction(exit)
+        return alert
+    }
+}
