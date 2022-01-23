6e7da12262a0bff97fafed634a1401b1fe77b955
Oleg991 <o.n.eremenko@gmail.com>
2022-01-23 15:39:38 +0300

    Updated assets and extensions

--- Days 2.1/Support/Extensions/UIColor+.swift
+++ Days 2.1/Support/Extensions/UIColor+.swift
@@ -1,8 +1,8 @@
 import UIKit.UIColor
 
 extension UIColor {
-    static let mainBackground = UIColor(named: "MainBackgroundColor")!
-    static let buttonTint     = UIColor(named: "ButtonTint")!
-    static let mainTitle      = UIColor(named: "MainTitleColor")!
-    static let textColor      = UIColor(named: "TextColor")!
+    static let mainBackground   = UIColor(named: "MainBackgroundColor")!
+    static let buttonTint       = UIColor(named: "ButtonTintColor")!
+    static let textColor        = UIColor(named: "TextColor")!
+    static let buttonBackground = UIColor.systemYellow
 }
--- Days 2.1/Support/Extensions/UIImage+.swift
+++ Days 2.1/Support/Extensions/UIImage+.swift
@@ -1,14 +1,36 @@
 import UIKit.UIImage
 
 extension UIImage {
-    func tint(with color: UIColor) -> UIImage {
-        var image = withRenderingMode(.alwaysTemplate)
-        UIGraphicsBeginImageContextWithOptions(size, false, scale)
-        color.set()
+    func colored(_ color: UIColor) -> UIImage {
+        UIGraphicsImageRenderer(size: size).image { context in
+            color.setFill()
+            self.draw(at: .zero)
+            context.fill(
+                CGRect(
+                    x: .zero,
+                    y: .zero,
+                    width: size.width,
+                    height: size.height
+                ),
+                blendMode: .sourceAtop
+            )
+        }
+    }
+}
 
-        image.draw(in: CGRect(origin: .zero, size: size))
-        image = UIGraphicsGetImageFromCurrentImageContext()!
+public extension UIImage {
+    convenience init?(
+        color: UIColor,
+        size: CGSize = .init(width: 1, height: 1)
+    ) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, .zero)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return image
+
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
     }
 }
